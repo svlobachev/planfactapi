2ecb92e6595156446da5eba6452fe17ee1938a43
Sergei <gsvlobachev@gmail.com>
2021-09-25 15:05:12 +0300

    7 добавлены поля на страницу настроек плагина + сохранение в БД ключа от ПланФакт

--- includes/class-admin-core.php
+++ includes/class-admin-core.php
@@ -38,22 +38,6 @@ class Planfact_API_core{
         ));
         $obj_response = json_decode($response['body'] );
         return $obj_response ;
-
-//        stdClass Object
-//        (
-//            [data] => stdClass Object
-//            (
-//                [apiKey] => HuOltjHipb34TsJG94Irv9iHe_6SljEreQ1V_xQBonzE42IH4PrMXbkwt6zEId0YL2n72x3F8asFYHPKAjviTiAzBP4HV03ac9NKta5aeOt35k-ORdRXcUlYQwPtdjmimRv7evDhOl8s8qOsQwHFK9BosEqX4-fMGIJomQu8L6Vus_uaySJjecA0b2KWYYgt2MWxIOUDk-Us57WdVcYGENHELrV2Nrcc83-_BH9DMXdhOXH4Q8HPNOTyiDfPAz1F6_IvBwRoefRVTAVwwblwbvQ8shai-RB_gp7H1bxS-_Fkz43ULRp213sW6PH-xbA2zupbWI5NS33uWwyE8UI5U851MUxbemwg1yIHO0_DWrwdwyHlZI0NOaLqpoAZSGAvkPWVyKWj75kxmaQaGlbnZBq9LCqM9HG6J01UheN-fF1jsz4y0NycQJzhqw_yyO6jDBKA2XJ2N50wQRWKrmcUFDcQDkShV35PB7bKwjknPgaXnsS5pJWSKFvB0b3swXbV8MoZcel19Iym_q7_6fjMB44xzzE0QqkxH_uRCbkBLTFj46NNBxfUc1gqj872Sq6M6p9ke_ky3rZKqAU5Mx-aduGzTI8
-//                [businessId] => 37279758-9254-4a21-95db-c7166c4e378f
-//                [businessTitle] =>
-//            )
-//
-//            [isSuccess] => 1
-//            [errorMessage] =>
-//            [errorCode] =>
-//        )
-
-//        http://localhost/wp-login.php?checkemail=registered
     }
 
 }
